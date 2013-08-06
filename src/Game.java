6081dd72f82e7ae7fbc7ff532f9213a8874a52a5
Eskimo-NZ <alex.candler@gmail.com>
2013-08-07 10:00:08 +1200

    Added Methods to Game

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,20 @@
 
 public class Game {
 	private String currentTurn;
+	
 	public Game(){
+		Board board = new Board();
+		
+		//Ask for number of players
+		// Create all players, list of players?
+		Player player = new Player();
+		
+		//While dice roll is zero or more (must include zero as they can still accuse)
+		
+		board.getOptions(); // Gets all potential options for the player
+		
+		player.presentOptions(); // Presents all the options to the player
+		
 		
 	}
 	
