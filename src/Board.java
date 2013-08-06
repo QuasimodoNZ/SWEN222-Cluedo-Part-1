0827a1ff3c93015d4395d78d87ee2d2038d6cdcc
Eskimo-NZ <alex.candler@gmail.com>
2013-08-07 10:52:20 +1200

    Removed Game, Updated Board methods

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,25 @@
 public class Board {
 
 	public Board() {
+		// Ask for number of players
+		// Create all players, list of players?
+		Player player = new Player("Jack Mustard");
 
+		// While dice roll is zero or more (must include zero as they can still
+		// accuse)
+
+		// Gets all potential options for the player
+		String options = getOptions(player); 
+
+		// Presents all the options to the player
+		while (true){
+			String selectedOption = player.presentOptions(options); 
+	
+			if (play(selectedOption)){
+				System.out.println("Successful Play");
+				break;
+			} 
+		}
 	}
 
 	public String getOptions(Player player) {
@@ -14,5 +32,18 @@ public class Board {
 		String options = null;
 
 		return options;
+	}
+	
+	/**
+	 * Makes the requested play, returns true if successful
+	 * @param The option requested
+	 * @return True if successful, false otherwise
+	 */
+	public boolean play(String option){
+		return true;	
+	}
+	
+	public static void main(String[] args) {
+		Board board = new Board();
 	}
 }
