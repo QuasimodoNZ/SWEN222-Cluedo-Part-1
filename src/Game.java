938a3bd7360f21d25a2df617c25eb7cb97e20bdc
Benjamin Riddell <benjamin.riddell@hotmail.com>
2013-08-06 22:00:08 +1200

    Cheese update

--- src/Game.java
+++ src/Game.java
@@ -2,12 +2,13 @@
 public class Game {
 	private String name;
 	public Game(){
-<<<<<<< HEAD
 		name = "ben's commit";
-=======
 		name = "alex's commit number 2";
->>>>>>> 48f526c65ff6db1bdfd3e2acb203057f71c6e458
 		
+	}
+	
+	public boolean eatCheese(){
+		return true;
 	}
 	
 	public static void main(String[] args) {
