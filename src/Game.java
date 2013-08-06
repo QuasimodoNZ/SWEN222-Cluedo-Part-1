aae492488319f9a13c7c983bb182d50d9102e68b
Benjamin Riddell <benjamin.riddell@hotmail.com>
2013-08-06 21:18:15 +1200

    Let there be light
    Point of creation

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,13 @@
 
 public class Game {
-
+	private String name;
+	public Game(){
+		name = "test";
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Game obj = new Game();
 	}
 
 }
