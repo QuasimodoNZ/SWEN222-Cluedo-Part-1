1c2d9b0caac83171551e2204c1763ef9ce55bd56
Benjamin Riddell <benjamin.riddell@hotmail.com>
2013-08-08 00:08:32 +1200

    Added location to Player
    Added the ability for a Player to store its location and to be able to
    move to a specified location using 2 new methods:
    - getLocation()
    - moveLocation(Location)

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,19 @@
 public class Player {
 	private Board.Character character;
+	private Location location;
 
 	public Player(Board.Character character) {
 		this.character = character;
 
+	}
+
+	/**
+	 * Returns the location that this player is on
+	 * 
+	 * @return Location
+	 */
+	public Location getLocation() {
+		return location;
 	}
 
 	public Board.Character getCharacter() {
@@ -14,5 +24,16 @@ public class Player {
 		// TODO Finish Method
 		String option = null;
 		return option;
+	}
+
+	/**
+	 * Removes its current location's reference to the player and moves it to
+	 * the specified location
+	 * 
+	 * @param nextLocation
+	 */
+	public void moveLocation(Location nextLocation) {
+		location.setCharacter(null);
+		location = nextLocation;
 	}
 }
