445831b2f31b87747d6427deeb1747352ed955a1
Eskimo-NZ <alex.candler@gmail.com>
2013-08-12 11:12:53 +1200

    Removed Unused Tests

--- src/CludoTests.java
+++ src/CludoTests.java
@@ -35,43 +35,4 @@ public class CludoTests {
 				Board.Character.ELEANOR_PEACOCK, true).toString());
 	}
 
-	/*
-	 * @Test public void testVaildStartBoard() { Board b = new Board();
-	 * b.startBoard(); String line =
-	 * "3\njack mustard\njacob green\nvictor plum"; InputStream input = new
-	 * ByteArrayInputStream(line.getBytes() ); System.setIn(input); List<Player>
-	 * p = b.getPlayers(); assertTrue(p.size() == 6); }
-	 */
-	@Test
-	public void testValidMove() {
-		Board b = createBoard();
-		Player p1 = b.getPlayers().get(0);
-		Player p2 = b.getPlayers().get(1);
-		Location moveLoc = new Location(new Point(6, 1), null);
-
-		b.move(p1, "north");
-		assertEquals(p1.getLocation(), moveLoc);
-	}
-
-	/**
-	 * Creates a new Board, adds two players, creates two locations and adds
-	 * them to a hallway
-	 * 
-	 * @return
-	 */
-	public Board createBoard() {
-		Board b = new Board();
-		Player p1 = new Player(Board.Character.ELEANOR_PEACOCK, true);
-		Player p2 = new Player(Board.Character.JACOB_GREEN, true);
-		Location p1Loc = new Location(new Point(6, 0), null);
-		Location p2Loc = new Location(new Point(0, 9), null);
-		p1.moveLocation(p1Loc);
-		p2.moveLocation(p2Loc);
-		List<Location> locList = new ArrayList<Location>();
-		locList.add(p1Loc);
-		locList.add(p2Loc);
-		// Room hallway = new Room(Board.RoomName.HALLWAY, locList);
-		return b;
-	}
-
 }
